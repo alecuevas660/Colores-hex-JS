2a4fe604b184547b895797fa13a3a32604208b07
alecuevas660 <alecuevas660@gmail.com>
2023-08-03 23:46:17 -0400

    Multicolores Freecodecamp

---
+++ js/js.js
@@ -0,0 +1,24 @@
+//Seleccionar elementos del dom
+
+
+const boton = document.querySelector('button');
+
+const color = document.getElementById('color');
+
+function generarColor(){
+    let digito = "0123456789ABCDEF"
+    let colorHex = "#"
+
+    for (let i = 0 ;i < 6; i++){
+        let numAleatorio = Math.floor(Math.random()*16)
+        colorHex += digito[numAleatorio];
+    }
+    return colorHex;
+}
+
+
+boton.addEventListener('click', function(){
+    let colorAleatorio = generarColor();
+    color.textContent = colorAleatorio;
+    document.body.style.backgroundColor = colorAleatorio;
+})
